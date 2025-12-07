1eaa70ad9d073706cb8b8a4eaa9bfc712b0108c6
Neil Kiuchi <neil_kiuchi@telecom-sudparis.eu>
2025-12-07 23:05:06 +0100

    Revert src/app/app.config.ts to main version

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -13,7 +12,6 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideAnimationsAsync(),
     {
       provide: MAT_ICON_DEFAULT_OPTIONS,
       useValue: {
