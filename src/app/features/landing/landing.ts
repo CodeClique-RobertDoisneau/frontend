cbd67c303c9e0ddd9155914818de00bed59823e8
TimotheCM <timothecm.dev@gmail.com>
2026-02-12 19:06:01 +0100

    feat: add horizontal slider to the landing page

--- src/app/features/landing/landing.ts
+++ src/app/features/landing/landing.ts
@@ -3,10 +3,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { HorizontalSlider } from '@shared/components/horizontal-slider/horizontal-slider';
 
 @Component({
   selector: 'app-landing',
-  imports: [MatButtonModule, MatCardModule, MatIconModule, RouterLink],
+  imports: [MatButtonModule, MatCardModule, MatIconModule, RouterLink, HorizontalSlider],
   templateUrl: './landing.html',
   styleUrl: './landing.scss',
 })
