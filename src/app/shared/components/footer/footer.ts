fa010e3be188c9e8655bb3bee51112927043750a
Dimitri FAJAL <dimitri.faj@orange.fr>
2026-02-12 21:06:28 +0100

    feat: Added logo, use matButton

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-footer',
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatButtonModule, MatDividerModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
