7241c22ed525829c0db0533e68f826a94d7d440a
Dimitri FAJAL <dimitri.faj@orange.fr>
2026-02-12 19:26:18 +0100

    fix: added trust

--- src/app/shared/components/persona/persona.ts
+++ src/app/shared/components/persona/persona.ts
@@ -1,19 +1,24 @@
-import { Component, input, computed } from '@angular/core';
+import { Component, input, computed, inject, SecurityContext } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-persona',
-  imports: [],
   templateUrl: './persona.html',
-  styleUrl: './persona.scss',
 })
 export class Persona {
-  subject = input<String>('Maths');
-  pose = input<String>('Default');
+  private sanitizer = inject(DomSanitizer);
 
+  subject = input<string>('Maths');
+  pose = input<string>('Default');
 
-
-  imagePath = computed(() => {
-    'img/persona/' + this.subject() + '-' + this.pose() + '.webp'
+  // Trust the URL for the [data] binding
+  imagePath = computed<SafeResourceUrl>(() => {
+    const url = `img/persona/${this.subject()}-${this.pose()}.webp`;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   });
-}
 
+  // Create a dynamic description for accessibility
+  personaDesc = computed(() => 
+    `Illustration of a ${this.subject()} teacher in a ${this.pose()} pose`
+  );
+}
