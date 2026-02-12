ed1748d7ee78b73454a4556d5166468b729e9bd5
TimotheCM <timothecm.dev@gmail.com>
2026-02-12 18:57:21 +0100

    fix: add link to backend

--- src/app/shared/components/chapter-card/chapter-card.ts
+++ src/app/shared/components/chapter-card/chapter-card.ts
@@ -1,15 +1,43 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, effect, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { httpResource } from '@angular/common/http';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
+
+export interface ChapterInfo {
+  id: number;
+  title: string;
+  description: string;
+  grade_level: string;
+  created_at: Date;
+  modified_at: Date;
+  sections: SectionInfo[];
+}
+
+
+export interface SectionInfo {
+  id: number;
+  title: string;
+  difficulty: number;
+}
 
 @Component({
   selector: 'app-chapter-card',
-  imports: [CommonModule, MatCardModule, MatButtonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatProgressBarModule],
   templateUrl: './chapter-card.html',
   styleUrl: './chapter-card.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChapterCard {
-  item = input<any>();
+  //id du chapitre
+  id = input<number>();
+
+  apiPath = computed(() => {
+    const id = this.id();
+    return id ? `http://localhost/api/chapter/${id}/` : undefined;
+  });
+
+  chapterInfo = httpResource<ChapterInfo>(() => this.apiPath());
 }
--- src/app/shared/components/horizontal-slider/horizontal-slider.ts
+++ src/app/shared/components/horizontal-slider/horizontal-slider.ts
@@ -1,32 +1,36 @@
-import { Component, ElementRef, input, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ChapterCard } from '../chapter-card/chapter-card';
-import { MatAnchor, MatButtonModule } from '@angular/material/button';
+import { ChapterCard, ChapterInfo } from '../chapter-card/chapter-card';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { httpResource } from '@angular/common/http';
 
 @Component({
   selector: 'app-horizontal-slider',
   templateUrl: './horizontal-slider.html',
   styleUrls: ['./horizontal-slider.scss'],
-  imports: [ChapterCard, CommonModule, MatAnchor, MatButtonModule, MatIconModule],
+  imports: [ChapterCard, CommonModule, MatButtonModule, MatIconModule],
 })
 export class HorizontalSlider {
-  // On reçoit la liste des éléments (films, produits, etc.)
-  items = input<any>();
+  // Lien api pour trouver la liste des chapitres
+  apiPath = computed(() => 'http://localhost/api/chapter/');
+
+  // Récupère les chapitres
+  chapters = httpResource<ChapterInfo[]>(() => this.apiPath());
 
   // On cible l'élément HTML qui a l'id #sliderContent
   @ViewChild('sliderContent') sliderContent!: ElementRef;
 
   scrollLeft() {
     this.sliderContent.nativeElement.scrollBy({
-      left: -600, // Distance de défilement vers la gauche
+      left: -600,
       behavior: 'smooth',
     });
   }
 
   scrollRight() {
     this.sliderContent.nativeElement.scrollBy({
-      left: 600, // Distance de défilement vers la droite
+      left: 600,
       behavior: 'smooth',
     });
   }
