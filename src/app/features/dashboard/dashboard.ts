2e191892547de1b11e7eb5a57c9f0aab576a1e95
Dimitri FAJAL <dimitri.faj@orange.fr>
2026-02-12 20:41:24 +0100

    chore: moved instances from landing to dashboard  and courses-catalog

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HorizontalSlider } from '@shared/components/horizontal-slider/horizontal-slider';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [HorizontalSlider],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
--- src/app/features/landing/landing.ts
+++ src/app/features/landing/landing.ts
@@ -3,11 +3,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
-import { HorizontalSlider } from '@shared/components/horizontal-slider/horizontal-slider';
 
 @Component({
   selector: 'app-landing',
-  imports: [MatButtonModule, MatCardModule, MatIconModule, RouterLink, HorizontalSlider],
+  imports: [MatButtonModule, MatCardModule, MatIconModule, RouterLink],
   templateUrl: './landing.html',
   styleUrl: './landing.scss',
 })
