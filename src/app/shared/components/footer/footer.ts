d6a70d30d1e9ea4b8b0dbe5587442bf186568d76
Dimitri FAJAL <dimitri.faj@orange.fr>
2026-03-12 22:37:48 +0100

    feat: Make footer use new logo and Material buttons

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-footer',
-  imports: [MatIconModule, MatButtonModule, MatDividerModule],
+  imports: [MatIconModule, MatButtonModule, MatDividerModule, RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
