9dc3c69027aa2bcce48fa897cbf8cc2463302400
Dimitri FAJAL <dimitri.faj@orange.fr>
2026-05-25 19:52:22 +0200

    feat(settings): design and implement settings page with sub-navigation and layout

--- src/app/features/settings/settings.ts
+++ src/app/features/settings/settings.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+type Tab = 'profile' | 'security' | 'classes' | 'notifications' | 'data';
 
 @Component({
   selector: 'app-settings',
-  imports: [],
+  imports: [MatButtonModule, MatCardModule, MatIconModule, MatListModule],
   templateUrl: './settings.html',
   styleUrl: './settings.scss',
 })
 export class Settings {
+  activeTab = signal<Tab>('profile');
 }
